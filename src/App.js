3287246723f4b4cfdf713c2f9a92db95f2b0c29f
Shilpa3107 <shilpa.sinha3107@gmail.com>
2024-08-24 02:19:48 +0530

    Fixed the issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import WalletConnect from './components/WalletConnect';
+import './App.css'; // If you have a CSS file for styling
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Crypto Wallet Connection</h1>
+        <WalletConnect />
       </header>
     </div>
   );
